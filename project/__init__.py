b18dfd91c290dbdcd3170c811d4ac46bd888be01
James <thenetimp@gmail.com>
2020-05-18 03:07:30 +0900

    setup flask migrations to work correctly

--- project/__init__.py
+++ project/__init__.py
@@ -6,8 +6,11 @@ def create_app(config_filename):
   app.config.from_envvar('FLASK_APP_CONFIG')
 
   from project.database import db
+  import project.models
+
   db.init_app(app)
   migrate = Migrate(app, db)
+
 
   @app.route('/')
   def index():
