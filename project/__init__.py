0e538be601eddbc6bee4c1ca4476036c887ad4ac
James <thenetimp@gmail.com>
2020-05-18 02:51:40 +0900

    moved files around

--- project/__init__.py
+++ project/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask 
+from flask_migrate import Migrate
 
 def create_app(config_filename):
   app = Flask(__name__)
   app.config.from_envvar('FLASK_APP_CONFIG')
 
-  from project.model import db
+  from project.database import db
   db.init_app(app)
+  migrate = Migrate(app, db)
 
   @app.route('/')
   def index():
